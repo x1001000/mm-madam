7e8e65c32a1a89cdbb4155299da21d43d842af40
PHIL <x1001000@gmail.com>
2025-04-29 01:23:34 +0800

    site_languages / subheader_texts / subdomains / lang_routes

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,8 +71,7 @@ def get_relevant_ids(csv_df_json) -> str:
         print(csv_df_json, result)
         return result
 
-def get_retrieval(knowledge_type) -> str:
-    csv_file = sorted(glob.glob(f'{knowledge_type}*.csv'))[-1]
+def get_retrieval(csv_file) -> str:
     try:
         ids = json.loads(get_relevant_ids(csv_file + ' => df.iloc[:,:2].to_json'))
     except json.JSONDecodeError as e:
@@ -80,54 +79,51 @@ def get_retrieval(knowledge_type) -> str:
         ids = None
     if ids:
         if type(ids[0]) is dict:
-            ids = [int(id_['id']) for id_ in ids]
+            ids = [int(_id['id']) for _id in ids]
         else:
-            ids = [int(id_) for id_ in ids]
+            ids = [int(_id) for _id in ids]
 
+        if user_prompt_type == '1':
+            df = st.session_state.knowledge[csv_file]
+            df = df[df['id'].isin(ids)]
         if user_prompt_type == '2':
             df = pd.DataFrame(columns=['id', 'html'])
             df['id'] = ids
             htmls = []
-            for id_ in ids:
-                with open(glob.glob(f'{knowledge_type}*{id_}.html')[0]) as f:
+            for _id in ids:
+                with open(csv_file.replace('_log', str(_id)).replace('csv', 'html')) as f:
                     htmls.append(''.join(f.readlines()))
             df['html'] = htmls
-            return df.to_json(orient='records', force_ascii=False)
-
-        df = st.session_state.knowledge[csv_file]
-        select_rows = df['id'].isin(ids)
-        # quickie, blog, edm
-        if 'date' in df.columns:
-            select_rows = select_rows & (df['date'] > after)
-            # exclude en blog posts of df['markdown_tc'].isna()
-            select_rows = select_rows & df['markdown_tc'].notna()
-        return df[select_rows].to_json(orient='records', force_ascii=False)
+        return df.to_json(orient='records', force_ascii=False)
     else:
         return ''
 
 def initialize_client():
     st.session_state.client = genai.Client(api_key=st.secrets['GEMINI_API_KEY'])
 
-if 'client' not in st.session_state:
-    st.session_state.contents = []
-    st.session_state.knowledge = {}
-    for csv_file in glob.glob('knowledge/*.csv') + glob.glob('knowledge/*/*/*.csv'):
-        df = pd.read_csv(csv_file)
-        st.session_state.knowledge[csv_file] = df
-        st.session_state.knowledge[csv_file + ' => df.iloc[:,:2].to_json'] = df.iloc[:,:2].to_json(orient='records', force_ascii=False)
-    with st.container():
-        st.subheader("財經時事相關問題，例如：美債殖利率為何飆升？")
-        user_prompt = st.chat_input('Ask Madam', on_submit=initialize_client)
-else:
-    client = st.session_state.client
-    # When st.chat_input is used in the main body of an app, it will be pinned to the bottom of the page.
-    user_prompt = st.chat_input('Ask Madam')
+site_languages = [
+    '繁體中文',
+    '简体中文',
+    'English']
+subheader_texts = [
+    "財經時事相關問題，例如：美債殖利率為何飆升？",
+    "财经时事相关问题，例如：美债收益率为何飙升？",
+    "Financial and economic questions, e.g.: Why are US Treasury yields surging?"]
+subdomains = [
+    'www',
+    'sc',
+    'en']
+lang_routes = [
+    'zh-tw',
+    'zh-cn',
+    'en-001']
 
 with st.sidebar:
     st.title('👩🏻‍💼 MM Madam')
     st.link_button('系統提示詞共筆，原則只增不刪，如需刪除請以註解方式說明原因，編輯同時問答立即生效，無需重新整理此網頁', 'https://docs.google.com/document/d/1HOS7nntBTgfuSlUpHgDIfBed5M_bq4dH0H8kqXUO9PE/edit?usp=sharing', icon='📝')
     st.link_button('請協助使用優化過的系統提示詞，對題庫進行一輪實測，複製貼上AI生成答覆，提供AI專案會議討論', 'https://docs.google.com/spreadsheets/d/1pe3d54QEyU0xQ_vJe_308UK9FzLYQJl7EQZkSyYgLeA/edit?usp=sharing', icon='💬')
     st.markdown('---')
+    site_language = st.radio('網站語系', site_languages, horizontal=True)
     is_paid_user = st.toggle('💎 付費用戶', value=True)
     has_chart = st.toggle('📊 MM圖表', value=is_paid_user, disabled=not is_paid_user)
     has_quickie = st.toggle('💡 MM短評', value=is_paid_user, disabled=not is_paid_user)
@@ -147,8 +143,29 @@ if has_memory:
         with st.chat_message(content.role, avatar=None if content.role == "user" else '👩🏻‍💼'):
             st.markdown(content.parts[0].text)
 else:
+    # initialize the conversation history when has_memory defaults to False
     # clear the conversation history
     st.session_state.contents = []
+
+if 'client' not in st.session_state:
+    with st.container():
+        subheader_text = dict(zip(site_languages, subheader_texts))[site_language]
+        st.subheader(subheader_text)
+        user_prompt = st.chat_input('Ask Madam', on_submit=initialize_client)
+else:
+    client = st.session_state.client
+    # When st.chat_input is used in the main body of an app, it will be pinned to the bottom of the page.
+    user_prompt = st.chat_input('Ask Madam')
+
+if 'knowledge' not in st.session_state:
+    st.session_state.knowledge = {}
+    for csv_file in glob.glob('knowledge/*.csv') + glob.glob('knowledge/*/*/*.csv'):
+        df = pd.read_csv(csv_file)
+        # quickie, blog, edm
+        if 'date' in df.columns:
+            df = df[df['date'] > after]
+        st.session_state.knowledge[csv_file] = df
+        st.session_state.knowledge[csv_file + ' => df.iloc[:,:2].to_json'] = df.iloc[:,:2].to_json(orient='records', force_ascii=False)
 
 system_prompt = requests.get(st.secrets['SYSTEM_PROMPT_URL']).text
 if user_prompt:
@@ -158,38 +175,39 @@ if user_prompt:
 
     user_prompt_type = get_user_prompt_type()
     if user_prompt_type == '1':
+        subdomain = dict(zip(site_languages, subdomains))[site_language]
         if not is_paid_user:
-            system_prompt += '\n\n- 你會鼓勵用戶升級成為付費用戶就能享有完整問答服務，並且提供訂閱方案連結 https://www.macromicro.me/subscribe 。'
+            system_prompt += f'\n\n- 你會鼓勵用戶升級成為付費用戶就能享有完整問答服務，並且提供訂閱方案連結 https://{subdomain}.macromicro.me/subscribe 。'
         if has_chart:
-            if retrieval := get_retrieval('knowledge/chart'):
-                system_prompt += '\n\n- 你會依據以下MM圖表的知識回答用戶提問，並且提供MM圖表連結 https://www.macromicro.me/charts/{id}/{slug} 。'
+            if retrieval := get_retrieval('knowledge/chart.csv'):
+                system_prompt += f'\n\n- 你會依據以下MM圖表的知識回答用戶提問，並且提供MM圖表連結 https://{subdomain}.macromicro.me/charts/{{id}}/{{slug}} 。'
                 system_prompt += '\n' + retrieval
-        if has_quickie:
-            if retrieval := get_retrieval('knowledge/quickie'):
-                system_prompt += '\n\n- 你會依據以下MM短評的知識回答用戶提問，並且提供MM短評連結 https://www.macromicro.me/quickie?id={id} 。'
+        # only zh-tw and zh-cn have quickie
+        if has_quickie and site_language in site_languages[:2]:
+            if retrieval := get_retrieval('knowledge/quickie.csv'):
+                system_prompt += f'\n\n- 你會依據以下MM短評的知識回答用戶提問，並且提供MM短評連結 https://{subdomain}.macromicro.me/quickie?id={{id}} 。'
                 system_prompt += '\n' + retrieval
-        if has_blog:
-            if retrieval := get_retrieval('knowledge/blog'):
-                system_prompt += '\n\n- 你會依據以下MM部落格的知識回答用戶提問，並且提供MM部落格連結 https://www.macromicro.me/blog/{slug} 。'
+        if has_blog and site_language in site_languages[:2]:
+            if retrieval := get_retrieval('knowledge/blog.csv'):
+                system_prompt += f'\n\n- 你會依據以下MM部落格的知識回答用戶提問，並且提供MM部落格連結 https://{subdomain}.macromicro.me/blog/{{slug}} 。'
+                system_prompt += '\n' + retrieval
+        if has_blog and site_language == 'English':
+            if retrieval := get_retrieval('knowledge/blog_en.csv'):
+                system_prompt += f'\n\n- 你會依據以下MM部落格的知識回答用戶提問，並且提供MM部落格連結 https://{subdomain}.macromicro.me/blog/{{slug}} 。'
                 system_prompt += '\n' + retrieval
         if has_edm:
-            if retrieval := get_retrieval('knowledge/edm'):
-                system_prompt += '\n\n- 你會依據以下MM獨家報告的知識回答用戶提問，並且提供MM獨家報告連結 https://www.macromicro.me/mails/monthly_report 。'
+            if retrieval := get_retrieval('knowledge/edm.csv'):
+                system_prompt += f'\n\n- 你會依據以下MM獨家報告的知識回答用戶提問，並且提供MM獨家報告連結 https://{subdomain}.macromicro.me/mails/monthly_report 。'
                 system_prompt += '\n' + retrieval
         if has_stocks:
-            system_prompt += '\n\n- 若用戶或你提及美國上市公司，你會提供MM美股財報資料庫中該公司的連結 https://www.macromicro.me/stocks/info/{股票代號} 。'
+            system_prompt += f'\n\n- 若用戶或你提及美國上市公司，你會提供MM美股財報資料庫中該公司的連結 https://{subdomain}.macromicro.me/stocks/info/{{股票代號}} 。'
         # if has_search:
         #     system_prompt += '\n\n- 你最終會以Google搜尋做為事實依據回答用戶提問。'
     if user_prompt_type == '2':
         if has_hc:
-            if retrieval := get_retrieval('knowledge/hc*/zh-tw/'):
-                system_prompt += '\n\n- 你會依據以下MM幫助中心的知識回答用戶提問，並且提供MM幫助中心連結 https://support.macromicro.me/hc/zh-tw/articles/{id} 。'
-                system_prompt += '\n' + retrieval
-            elif retrieval := get_retrieval('knowledge/hc*/en-001/'):
-                system_prompt += '\n\n- You will answer user inquiries based on the knowledge as follows and provide the link to the MM Help Center. https://support.macromicro.me/hc/en-001/articles/{id} 。'
-                system_prompt += '\n' + retrieval
-            elif retrieval := get_retrieval('knowledge/hc*/zh-cn/'):
-                system_prompt += '\n\n- 你會依據以下MM幫助中心的知識回答用戶提問，並且提供MM幫助中心連結 https://support.macromicro.me/hc/zh-cn/articles/{id} 。'
+            lang_route = dict(zip(site_languages, lang_routes))[site_language]
+            if retrieval := get_retrieval(f'knowledge/hc/{lang_route}/_log.csv'):
+                system_prompt += f'\n\n- 你會依據以下MM幫助中心的知識回答用戶提問，並且提供MM幫助中心連結 https://support.macromicro.me/hc/{lang_route}/articles/{{id}} 。'
                 system_prompt += '\n' + retrieval
     if user_prompt_type == '3':
         system_prompt += '\n\n- 若非財經時事相關問題，你會婉拒回答。'
